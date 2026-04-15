4bc6706cd23a3440fd0a63edb898aa8526ad1ad1
Luka <dlukatomas@gmail.com>
2026-04-15 20:57:23 -0300

    agrego preview catalogos mobile

--- src/components/Catalogo.jsx
+++ src/components/Catalogo.jsx
@@ -5,16 +5,28 @@ function Catalogo() {
         <h2>Catálogos Bagués</h2>
 
         <div className="catalogos-grid">
-          
+
           {/* CATALOGO 1 */}
           <div className="catalogo-card">
             <h3>CATÁLOGO GENERAL</h3>
 
-            <iframe
-              src="/catalogo1.pdf#toolbar=0"
-              title="Catalogo 1"
-              className="pdf"
-            ></iframe>
+            <div className="pdf-container">
+              {/* PC */}
+              <iframe
+                src="/catalogo1.pdf#toolbar=0"
+                title="Catalogo 1"
+                className="pdf"
+              ></iframe>
+
+              {/* MOBILE */}
+              <div className="pdf-mobile">
+                <img src="/catalogo1.jpg" alt="Catálogo General" />
+
+                <a href="/catalogo1.pdf" target="_blank" className="btn">
+                  Ver catálogo completo
+                </a>
+              </div>
+            </div>
 
             <div className="catalogo-buttons">
               <a href="/catalogo1.pdf" target="_blank" className="btn">
@@ -31,11 +43,23 @@ function Catalogo() {
           <div className="catalogo-card">
             <h3>CATÁLOGO UNLOCK</h3>
 
-            <iframe
-              src="/catalogo2.pdf#toolbar=0"
-              title="Catalogo 2"
-              className="pdf"
-            ></iframe>
+            <div className="pdf-container">
+              {/* PC */}
+              <iframe
+                src="/catalogo2.pdf#toolbar=0"
+                title="Catalogo 2"
+                className="pdf"
+              ></iframe>
+
+              {/* MOBILE */}
+              <div className="pdf-mobile">
+                <img src="/catalogo2.jpg" alt="Catálogo Unlock" />
+
+                <a href="/catalogo2.pdf" target="_blank" className="btn">
+                  Ver catálogo completo
+                </a>
+              </div>
+            </div>
 
             <div className="catalogo-buttons">
               <a href="/catalogo2.pdf" target="_blank" className="btn">
