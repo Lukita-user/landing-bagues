5a3c4ba7bfea97e7eb652679764b3c14a10579fa
Luka <dlukatomas@gmail.com>
2026-06-23 01:33:41 -0300

    Nuevo ciclo

--- src/data/catalogos.js
+++ src/data/catalogos.js
@@ -3,12 +3,12 @@ export const catalogos = [
     id: 1,
     nombre: "Catálogo General",
     pdf: "/catalogo1.pdf",
-    portada: "https://i.imgur.com/szs8raP.jpeg",
+    portada: "https://i.imgur.com/8QOXu7O.png",
   },
   {
     id: 2,
     nombre: "Catálogo UNLOCK",
     pdf: "/catalogo2.pdf",
-    portada: "https://i.imgur.com/YmLkewn.jpeg",
+    portada: "https://i.imgur.com/nMRwMmQ.jpeg",
   },
 ];
