916ab1990cb4d4c6ffa6bf1e27bcc8f5d6eef47a
Luka <dlukatomas@gmail.com>
2026-07-24 22:20:37 -0300

    siglo 8

--- src/data/catalogos.js
+++ src/data/catalogos.js
@@ -3,12 +3,12 @@ export const catalogos = [
     id: 1,
     nombre: "Catálogo General",
     pdf: "/catalogo1.pdf",
-    portada: "https://i.imgur.com/8QOXu7O.png",
+    portada: "https://i.imgur.com/aLEVmpX.jpeg",
   },
   {
     id: 2,
     nombre: "Catálogo UNLOCK",
     pdf: "/catalogo2.pdf",
-    portada: "https://i.imgur.com/nMRwMmQ.jpeg",
+    portada: "https://i.imgur.com/lps5zpV.jpeg",
   },
 ];
