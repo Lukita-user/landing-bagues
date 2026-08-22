625df2c9e682520b2a3faf029366981629312284
Luka <dlukatomas@gmail.com>
2026-08-22 16:40:18 -0300

    catalogos nuevo cicloo

--- src/data/catalogos.js
+++ src/data/catalogos.js
@@ -3,14 +3,14 @@ export const catalogos = [
     id: 1,
     nombre: "Catálogo General",
     pdf: "/catalogo2.pdf",
-    portada: "https://i.imgur.com/aLEVmpX.jpeg",
+    portada: "https://i.imgur.com/McViN8N.jpeg",
   },
   {
     id: 2,
     nombre: "Catálogo UNLOCK",
     pdf: "/catalogo1.pdf",
-    portada: "https://i.imgur.com/lps5zpV.jpeg",
+    portada: "https://i.imgur.com/WEWqsG5.jpeg",
    },
 ];
 
-// RECORDAR VOLVER A PONER LOS ARVCHIVOS COMO ERAN 222uh29uhq9h
+// RECORDAR VOLVER A PONER LOS ARVCHIVOS COMO ERAN 22
